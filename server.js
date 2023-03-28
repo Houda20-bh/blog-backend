7bec743c58017fa52949ab8c51149f202a0e75ec
Houda <68481393+Houda20-bh@users.noreply.github.com>
2023-03-28 22:08:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,11 +15,9 @@ const app= express()
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use(cors());
-app.use((req, res, next) => {
+app.get("/",(req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.send(" api is running ");
-  next();
-  
+  res.send(" api is running "); 
 });
 //routes
 app.use('/api/blogs',require('./routes/blogRoutes'));
